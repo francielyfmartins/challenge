90e59b1ccc48d690408b1b6849a81db1cbc96dd0
Franciely Martins <francyf.martinss@gmail.com>
2025-03-10 00:07:40 -0300

    challenge

---
+++ app.js
@@ -0,0 +1,50 @@
+let amigos = [];
+
+function adicionarAmigo(){
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+
+    if (nomeAmigo == ""){
+        alert("Por favor, insira um nome.");
+        return;
+    } if(amigos.includes(nomeAmigo)){
+        alert(`O nome ${nomeAmigo} já está na lista.`);
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+
+    inputAmigo.value = "";
+
+    atualizarLista();
+}
+
+function atualizarLista(){
+    const listaAmigos = document.getElementById('listaAmigos');
+
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    
+    if(amigos.length == 0){
+        alert("Sem amigos para sortear, por favor adicione um nome de amigo.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo secreto sorteado é: <strong>${amigoSorteado}</strong>`;
+
+    // Limpa a lista de amigos após o sorteio
+    amigos = [];
+    atualizarLista();
+}
